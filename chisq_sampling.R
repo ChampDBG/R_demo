cf9297a9f80f06968bd39564dacf9f68fedffed2
Tung-Cheng-Wu <tung.cheng.wu@gmail.com>
2017-07-02 14:39:35 +0800

    Add R script
    adding the example about sampling from chi-square distribution.

---
+++ chisq_sampling.R
@@ -0,0 +1,23 @@
+### 建立一筆卡方分配的資料
+x <- rchisq(10000, df = 4)
+
+# 畫圖確認分配的樣子
+hist(x, freq = FALSE, ylim = c(0, 0.2))
+curve(dchisq(x, df = 4), col = 2, lty = 2, lwd = 2, add = TRUE)
+
+### 進行抽樣
+# 設定抽樣次數 (大約跑10秒，如果太久把數字改小)
+times <- 100000
+
+# 設定方資料的地方
+dta_s <- matrix(0, times)
+
+# 開始抽樣
+for (i in 1:times) {
+    x_s <- sample(x, 100)
+    dta_s[i] <- mean(x_s)
+}
+
+# 畫圖看結果
+hist(dta_s, freq = FALSE)
+lines(density(dta_s), col = 2, lty = 2, lwd = 2)
