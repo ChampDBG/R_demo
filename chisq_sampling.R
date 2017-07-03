52da3fcb905d19dc7a0b0eda766a77e27912f87d
Tung-Cheng-Wu <tung.cheng.wu@gmail.com>
2017-07-04 01:38:25 +0800

    update-chisq_sampling.r
    sampling by sd and sampling 30 times

--- chisq_sampling.R
+++ chisq_sampling.R
@@ -15,9 +15,27 @@ dta_s <- matrix(0, times)
 # 開始抽樣
 for (i in 1:times) {
     x_s <- sample(x, 100)
-    dta_s[i] <- mean(x_s)
+    dta_s[i] <- sd(x_s)
 }
 
 # 畫圖看結果
 hist(dta_s, freq = FALSE)
 lines(density(dta_s), col = 2, lty = 2, lwd = 2)
+
+### 改計算標準差
+for (i in 1:times) {
+    x_s <- sample(x, 100)
+    dta_s[i] <- sd(x_s)
+}
+hist(dta_s, freq = FALSE)
+lines(density(dta_s), col = 2, lty = 2, lwd = 2)
+
+### 只抽樣30次
+dta_s30 <- matrix(0, 30)
+
+for (i in 1:30) {
+    x_s <- sample(x, 100)
+    dta_s30[i] <- mean(x_s)
+}
+hist(dta_s30, freq = FALSE)
+lines(density(dta_s30), col = 2, lty = 2, lwd = 2)
